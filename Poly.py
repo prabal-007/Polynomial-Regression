01465d90cbc2c9c626df9f1f3a54bdc2ece7a2e1
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-12-05 17:26:35 +0530

    Update Poly.py

--- Poly.py
+++ Poly.py
@@ -1,4 +1,4 @@
-def PlotPolly(model, independent_variable, dependent_variabble, Name):
+def PlotPolly(model, independent_variable, dependent_variabble, x_label, y_label):
     x_new = np.linspace(15, 55, 100)
     y_new = model(x_new)
 
@@ -7,8 +7,8 @@ def PlotPolly(model, independent_variable, dependent_variabble, Name):
     ax = plt.gca()
     ax.set_facecolor((0.898, 0.898, 0.898))
     fig = plt.gcf()
-    plt.xlabel(Name)
-    plt.ylabel('Price of Cars')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
 
     plt.show()
     plt.close()
