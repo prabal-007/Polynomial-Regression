9a8996fba0c1cb66c49ac95ce2cbb16ddca3bc09
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-12-05 17:25:21 +0530

    Create Poly.py

---
+++ Poly.py
@@ -0,0 +1,14 @@
+def PlotPolly(model, independent_variable, dependent_variabble, Name):
+    x_new = np.linspace(15, 55, 100)
+    y_new = model(x_new)
+
+    plt.plot(independent_variable, dependent_variabble, '.', x_new, y_new, '-')
+    plt.title('Polynomial Fit with Matplotlib for Price ~ Length')
+    ax = plt.gca()
+    ax.set_facecolor((0.898, 0.898, 0.898))
+    fig = plt.gcf()
+    plt.xlabel(Name)
+    plt.ylabel('Price of Cars')
+
+    plt.show()
+    plt.close()
